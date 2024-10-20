e8209410d10e7eb8370b44ebb9d1d869ed685bde
Lagudas Joyce <143520079+joycekrystal@users.noreply.github.com>
2024-10-20 14:23:57 -0400

    Code Update

---
+++ my-photo-album/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Album from './Album';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Photo Album</h1>
+      <Album />
+    </div>
+  );
+}
+
+export default App;
